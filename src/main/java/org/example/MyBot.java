7e32fb5035e0fc6219cf66bdfee4e4c740970a80
Boss <badfreman0727@gmail.com>
2025-05-16 10:11:20 +0500

    maa ukaa

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -1,4 +1,35 @@
 package org.example;
 
-public class MyBot {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class MyBot extends TelegramLongPollingBot {
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String text = update.getMessage().getText();
+            Long chatId = update.getMessage().getChatId();
+
+            if (chatId.equals(chatId)) {
+
+            }
+
+        }
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "";
+    }
+
+
 }
