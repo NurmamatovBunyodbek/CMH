5f6840f524ec6856d3984c70aba93f2c0c94c988
iskandarovjavoxir <origamingjavoxir@gmail.com>
2025-05-19 22:58:14 +0500

    Buttons done

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -2,19 +2,33 @@ package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MyBot extends TelegramLongPollingBot {
 
 
     @Override
     public void onUpdateReceived(Update update) {
+        MyBotService myBotService = new MyBotService();
 
         if (update.hasMessage() && update.getMessage().hasText()) {
             String text = update.getMessage().getText();
             Long chatId = update.getMessage().getChatId();
 
-            if (chatId.equals(chatId)) {
+            if (text.equals("/start")) {
+                try {
+                    execute(myBotService.start(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
 
+            if (text.equals("To Do")) {
+                try {
+                    execute(myBotService.toDo(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
             }
 
         }
--- src/main/java/org/example/MyBotService.java
+++ src/main/java/org/example/MyBotService.java
@@ -1,5 +1,61 @@
 package org.example;
 
-public class MyBotService {
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyBotService {
+    public SendMessage start(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Salom, botimziga xush kelibsiz");
+
+        ReplyKeyboardMarkup reply = new ReplyKeyboardMarkup();
+        List<KeyboardRow> rowList = new ArrayList<>();
+        KeyboardRow row1 = new KeyboardRow();
+        KeyboardRow row2 = new KeyboardRow();
+
+        KeyboardButton button1 = new KeyboardButton("To Do");
+        KeyboardButton button2 = new KeyboardButton("In Progress");
+        KeyboardButton button3 = new KeyboardButton("Completed");
+
+        row1.add(button1);
+        row1.add(button2);
+        row2.add(button3);
+
+        rowList.add(row1);
+        rowList.add(row2);
+
+        reply.setKeyboard(rowList);
+        reply.setResizeKeyboard(true);
+        sendMessage.setReplyMarkup(reply);
+        return sendMessage;
+    }
+
+    public SendMessage toDo(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Yangi vazifa berish yoki Barcha vazifalarni ko'rish");
+
+        ReplyKeyboardMarkup reply = new ReplyKeyboardMarkup();
+        List<KeyboardRow> rowList = new ArrayList<>();
+        KeyboardRow row1 = new KeyboardRow();
+
+        KeyboardButton button1 = new KeyboardButton("New Task");
+        KeyboardButton button2 = new KeyboardButton("All Tasks");
+
+        row1.add(button1);
+        row1.add(button2);
+
+        rowList.add(row1);
+
+        reply.setKeyboard(rowList);
+        reply.setResizeKeyboard(true);
+        sendMessage.setReplyMarkup(reply);
+        return sendMessage;
+    }
 }
